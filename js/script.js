58ba2a9f2a36cf5b5d9ed82e03b6d93ed699a1fc
Mariia <milonigame1@gmail.com>
2023-03-22 16:56:18 +0300

    ready  access json server

--- js/script.js
+++ js/script.js
@@ -219,10 +219,23 @@ window.addEventListener('DOMContentLoaded', function() {
     };
 
     forms.forEach(item => {
-        postData(item);
+        bindpostData(item);
     });
 
-    function postData(form) {
+    const postData = async (url, data)=>{
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body:data
+        });
+
+        return await res.json();
+
+    };
+
+    function bindpostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
@@ -242,13 +255,8 @@ window.addEventListener('DOMContentLoaded', function() {
             });
             
 
-            fetch('server.php', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(object)
-            }).then(data =>data.text())
+         
+            postData('http://localhost:3000/requests', JSON.stringify(object))
             .then(data=>{
                 console.log(data);
                 showThanksModal(message.success);
