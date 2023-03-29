3508e2d564febe8361799c32d255800b1dde7c4d
Mariia <milonigame1@gmail.com>
2023-03-29 13:14:15 +0300

    ready slider with hide/show

--- js/script.js
+++ js/script.js
@@ -298,4 +298,65 @@ window.addEventListener('DOMContentLoaded', function() {
             closeModal();
         }, 4000);
     }
+
+
+    //Slider
+
+    let slideIndex = 1;
+    const slides = document.querySelectorAll('.offer__slide'),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current');
+
+    showSlides(slideIndex);
+
+    // if (slides.length < 10) {
+    //     total.textContent = `0${slides.length}`;
+    // } else {
+    //     total.textContent = slides.length;
+    // }
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        function slideHide(){
+            slides.forEach(item =>{
+                item.classList.add('hide');
+                item.classList.remove('show');
+            });
+            }
+            
+            function slideShow(){
+                slides[slideIndex - 1].classList.add('show'); // Как ваша самостоятельная работа - переписать на использование классов show/hide
+                slides[slideIndex - 1].classList.remove('hide');
+            }
+            slideHide();
+            slideShow();
+       
+        if (slides.length < 10) {
+            current.textContent =  `0${slideIndex}`;
+        } else {
+            current.textContent =  slideIndex;
+        }
+    }
+
+    function plusSlides (n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', function(){
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function(){
+        plusSlides(1);
+    });
 });
+    
+
