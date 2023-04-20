f86f317d0c6d9d28cb639fc26e5e185475d98c8d
Mariia <milonigame1@gmail.com>
2023-04-20 19:20:33 +0300

    common js setup

---
+++ main.js
@@ -0,0 +1,13 @@
+function myModule(){
+    this.hello=function(){
+        console.log('hello');
+    };
+
+    this.goodbye=function(){
+        console.log('bye!');
+        
+    };
+
+}
+
+module.exports=myModule;
