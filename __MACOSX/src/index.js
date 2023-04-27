3e49494cf4dc820ca57a9349b87054f33288739e
Mariia <milonigame1@gmail.com>
2023-04-27 13:04:36 +0300

    finish project

--- __MACOSX/src/index.js
+++
@@ -1,6 +0,0 @@
-const  myModule=require('js/main');
-
-const myModuleInstance=new myModule();
-
-myModuleInstance.hello();
-myModuleInstance.goodbye();
--- dist/main.js
+++
@@ -1,1 +0,0 @@
-(()=>{var o={693:o=>{o.exports=function(){this.hello=function(){console.log("hello")},this.goodbye=function(){console.log("bye!")}}}},e={};function n(t){var r=e[t];if(void 0!==r)return r.exports;var l=e[t]={exports:{}};return o[t](l,l.exports,n),l.exports}(()=>{const o=new(n(693));o.hello(),o.goodbye()})()})();
